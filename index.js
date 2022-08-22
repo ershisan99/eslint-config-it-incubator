c70fa664f80f6b4b13aa0f39e996a900702f9c95
Andres <andres99.dev@gmail.com>
2022-08-22 16:34:17 +0200

    update rules

--- index.js
+++ index.js
@@ -1,38 +1,39 @@
 module.exports = {
-    env: {
-      browser: true,
-      node: true,
-      amd: true,
-      es2021: true,
+  env: {
+    browser: true,
+    node: true,
+    amd: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'prettier/prettier',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    extends: [
-      'eslint:recommended',
-      'plugin:react/recommended',
-      'prettier/prettier',
-      'plugin:prettier/recommended',
-    ],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-      ecmaFeatures: {
-        jsx: true,
-      },
-      ecmaVersion: 12,
-      sourceType: 'module',
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'arrow-parens': 'off',
+    'no-nested-ternary': 'error',
+    'react/prop-types': 'off',
+    'react/display-name': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'no-empty-pattern': 'off',
+    'no-debugger': 'off',
+    'no-undef': 'off',
+    'no-unused-vars': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
     },
-    plugins: ['react', '@typescript-eslint'],
-    rules: {
-      'react/prop-types': 'off',
-      'react/display-name': 'off',
-      'react/react-in-jsx-scope': 'off',
-      'no-empty-pattern': 'off',
-      'no-debugger': 'off',
-      'no-undef': 'off',
-      'no-unused-vars': 'off',
-    },
-    settings: {
-      react: {
-        version: 'detect',
-      },
-    },
-  }
-  
+  },
+}
