3faea831c22d7bfbc5b8ffb1692270753b1af6e2
Andres <andres99.dev@gmail.com>
2022-08-23 12:56:28 +0200

    fix plugin

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ module.exports = {
     ecmaVersion: 12,
     sourceType: 'module',
   },
-  plugins: ['react', '@typescript-eslint'],
+  plugins: ['react', '@typescript-eslint', 'import'],
   rules: {
     'import/order': [
       'error',
